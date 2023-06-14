fc935179406006e601f2ae7859823cdb5df807f8
Denzel-007 <akinlaluredaniel@gmail.com>
2023-06-14 12:27:50 -0700

    Updated

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
+
 const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
-  }
+const ret = {};
+for (const key in dict) {
+  if (ret[dict[key]] === undefined) { ret[dict[key]] = []; }
+  ret[dict[key]].push(key);
 }
-console.log(newDict);
+console.log(ret);
